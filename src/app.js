6773945b75a59e17e84b41e52f4ffb5ab95f0633
juan <130606010+juancastrop@users.noreply.github.com>
2025-10-02 00:39:00 +0000

    creacion de variables y cartas principal

--- src/app.js
+++ src/app.js
@@ -8,4 +8,25 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  let numero = document.getElementById("numero")
+  let lista = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"]
+  let indiceLista = Math.floor(Math.random() * lista.length)
+  let elegido = lista[indiceLista]
+  numero.textContent = elegido
+
+  let listapinta = ["♦","♥","♠","♣"]
+  let indiceListaPinta = Math.floor(Math.random() * listapinta.length)
+  let elegidoPinta = listapinta[indiceListaPinta]
+  let pinta1 = document.getElementById("pinta1")
+  let pinta2 = document.getElementById("pinta2")
+  pinta1.textContent = elegidoPinta
+  pinta2.textContent = elegidoPinta
+
+  if (elegidoPinta == "♦" || elegidoPinta == "♥"){
+    pinta1.style.color = "red"
+    pinta2.style.color = "red"
+  } else {
+    pinta1.style.color = "black"
+    pinta2.style.color = "black"
+  } 
 };
